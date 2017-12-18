b4748624bb9657e14f5e7acbd01f5d01c16ddf66
Cuttle Cong <moyuyc95@gmail.com>
2017-12-18 15:37:31 +0800

    added .travis.yml

--- test/picidae.config.js
+++ test/picidae.config.js
@@ -10,18 +10,20 @@ module.exports = {
   docRoot: './doc',
   theme: '../',
   verbose: false,
-  webpackConfigUpdater(config) {
+  webpackConfigUpdater: function (config) {
     // config.module.loaders[2].include = [/\/node_modules\/picidae\//, nps.join(__dirname, '..')]
     config.module.loaders[2].exclude = [
       function (name) {
         return !/\/picidae-(plugin|transformer|theme|commander)/.test(name)
-          && !/(node_modules|bower_components)\/picidae/.test(name)
-          && /(node_modules|bower_components)/.test(name)
+               && !/(node_modules|bower_components)\/picidae/.test(name)
+               && /(node_modules|bower_components)/.test(name)
       },
       /\/node_modules\/(core-js|babel-runtime)\//
     ]
     return config
   },
+
+  distRoot: '../public'
 
   // templateData(gift) {
   //   console.log('templateData', gift)
