047099f517b7957dcda149e27762e45d0d0fde5f
cyseria <xcyseria@gmail.com>
2018-05-02 16:51:17 +0800

    更换about us内容

--- Readme.md
+++ Readme.md
@@ -1,3 +1,15 @@
 # eux-blog
 
+## start
+
+```bash
+npm install picidae -g
+
+npm run start
+# open http://localhost:8989
+
+# build the site for deploy
+npm run build
+```
+
 [![build status](https://img.shields.io/travis/be-fe/eux-blog/master.svg?style=flat-square)](https://travis-ci.org/be-fe/eux-blog)
--- theme/templates/AboutUS.js
+++ theme/templates/AboutUS.js
@@ -11,29 +11,50 @@ import PropTypes from 'picidae/exports/prop-types'
 import './assets/style/index.less'
 import './assets/style/about-us.less'
 
+const baseUrl = 'http://eux-blog-static.bj.bcebos.com/photo%2F';
+const baseExt = '@h_160,w_160';
 const About = ({ themeConfig }) => {
   const ours = themeConfig.ours.slice()
-
-  // 随机排序
-  ours.sort((a, b) => {
-    return Math.random() > 0.5 ? -1 : 1
-  })
   // 插入 logo 图
-  ours.splice(23, 0, { img: 'big', name: 'logo' })
+  ours.splice(28, 0, {name:'logo' , url: `aboutus-baidu.png`, hoverUrl: ''},)
 
-  // 默认图
-  const lists = ours.map((item, index) => {
-    if (item.name === 'logo') {
-      return (
-        <li key={index} className="big-logo"/>
-      )
+  const getSingleItem = function(item, index, className) {
+    let hoverUrl = '';
+    if (item.hoverUrl !== '') {
+      hoverUrl = <img className="avatar-hover" src={`${baseUrl}${item.hoverUrl}${baseExt}`} title={item.name} alt={item.name}/>
     }
-
     return (
-      <li key={index} data-title={item.name}>
-        <img className="avatar" src={item.img}/>
+      <li className={`${className}-item`} key={index} data-title={item.name}>
+        <img className="avatar" src={`${baseUrl}${item.url}${baseExt}`} title={item.name} alt={item.name}/>
+        {hoverUrl}
       </li>
     )
+  }
+
+  const ramdomColors = ['#E3B587', '#b0a085', '#e2bd3b', '#519670', '#caddd5'];
+  const lists = ours.map((item, index) => {
+    if (Object.prototype.toString.call(item) === '[object Array]') { // 联动的
+      const multipleItem = item.map((key, idx) => {
+        const itemKey = index + '-' + idx;
+        return getSingleItem(key, itemKey, 'multiple')
+      })
+      return <div className="multiple-items">{multipleItem}</div>;
+    } else { // 一个人
+      if (item.name === 'logo') {
+        return (
+          <li key={index} className="big-logo"/>
+        )
+      } else if (item.name === 'empty-item') {
+        const style = {
+          backgroundColor: ramdomColors[Math.floor(Math.random() * ramdomColors.length)]
+        };
+        return (
+          <li key={index} className='empty-item' style={style}/>
+        )
+      } else {
+        return getSingleItem(item, index, 'single');
+      }
+    }
   })
 
   return (
