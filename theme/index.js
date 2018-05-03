940ccb942ec47447c718aeafd5f80d5fcc6d7a5d
cyseria <xcyseria@gmail.com>
2018-05-03 10:26:55 +0800

    修改 about us 顺序

--- theme/index.js
+++ theme/index.js
@@ -96,15 +96,21 @@ module.exports = {
       {name:'赵蕊' , url: `%E8%B5%B5%E8%95%8A1.jpg`, hoverUrl: `%E8%B5%B5%E8%95%8A2.jpg`},
       {name:'唐婉琪' , url: `%E5%94%90%E5%A9%89%E9%AA%901.jpg`, hoverUrl: `%E5%94%90%E5%A9%89%E9%AA%902.jpg`},
       {name:'国悦婷' , url: `%E5%9B%BD%E6%82%A6%E5%A9%B7.jpeg`, hoverUrl: ``},
-      {name:'李兴彪' , url: `%E6%9D%8E%E5%85%B4%E5%BD%AA1.jpg`, hoverUrl: `%E6%9D%8E%E5%85%B4%E5%BD%AA2.jpg`},
+      {name:'empty-item' , url: '', hoverUrl: ''},
       {name:'孙甜甜' , url: `%E5%AD%99%E7%94%9C%E7%94%9C1.jpg`, hoverUrl: `%E5%AD%99%E7%94%9C%E7%94%9C2.jpg`},
       {name:'尤足强' , url: `%E5%B0%A4%E8%B6%B3%E5%BC%BA1.jpg`, hoverUrl: `%E5%B0%A4%E8%B6%B3%E5%BC%BA2.jpg`},
       {name:'康佳美' , url: `%E5%BA%B7%E4%BD%B3%E7%BE%8E1.jpg`, hoverUrl: `%E5%BA%B7%E4%BD%B3%E7%BE%8E2.jpg`},
-      {name:'张俊' , url: `%E5%BC%A0%E4%BF%8A1.jpg`, hoverUrl: `%E5%BC%A0%E4%BF%8A2.jpg`},
-      {name:'张宇良' , url: `%E5%BC%A0%E5%AE%87%E8%89%AF1.jpg`, hoverUrl: `%E5%BC%A0%E5%AE%87%E8%89%AF2.jpg`},
+      [
+        {name:'孙慧扬' , url: `%E5%AD%99%E6%85%A7%E6%89%AC1.jpg`, hoverUrl: `%E5%AD%99%E6%85%A7%E6%89%AC2.jpg`},
+        {name:'花云' , url: `%E8%8A%B1%E4%BA%911.jpg`, hoverUrl: `%E8%8A%B1%E4%BA%912.jpg`}
+      ],
       {name:'张超' , url: `%E5%BC%A0%E8%B6%851.jpg`, hoverUrl: `%E5%BC%A0%E8%B6%852.jpg`},
+      [
+        {name:'费千哲' , url: `%E8%B4%B9%E5%8D%83%E5%93%B21.jpg`, hoverUrl: `%E8%B4%B9%E5%8D%83%E5%93%B22.jpg`},
+        {name:'周轶景' , url: `%E5%91%A8%E8%BD%B6%E6%99%AF1.jpg`, hoverUrl: `%E5%91%A8%E8%BD%B6%E6%99%AF2.jpg`}
+      ],
       {name:'曹硕' , url: `%E6%9B%B9%E7%A1%951.jpg`, hoverUrl: `%E6%9B%B9%E7%A1%952.jpg`},
-      {name:'empty-item' , url: '', hoverUrl: ''},
+      {name:'赵晟孜' , url: `%E8%B5%B5%E6%99%9F%E5%AD%9C1.jpg`, hoverUrl: `%E8%B5%B5%E6%99%9F%E5%AD%9C2.jpg`},
       {name:'曾丽霞' , url: `%E6%9B%BE%E4%B8%BD%E9%9C%9E1.jpg`, hoverUrl: `%E6%9B%BE%E4%B8%BD%E9%9C%9E2.jpg`},
       {name:'李俚' , url: `%E6%9D%8E%E4%BF%9A1.jpg`, hoverUrl: `%E6%9D%8E%E4%BF%9A2.jpg`},
       {name:'孙传祥' , url: `%E5%AD%99%E4%BC%A0%E7%A5%A51.jpg`, hoverUrl: `%E5%AD%99%E4%BC%A0%E7%A5%A52.jpg`},
@@ -112,7 +118,10 @@ module.exports = {
       {name:'李欢' , url: `%E6%9D%8E%E6%AC%A21.jpg`, hoverUrl: `%E6%9D%8E%E6%AC%A22.jpg`},
       {name:'empty-item' , url: '', hoverUrl: ''},
       {name:'杨萌' , url: `%E6%9D%A8%E8%90%8C1.jpg`, hoverUrl: `%E6%9D%A8%E8%90%8C2.jpg`},
-      {name:'林枫' , url: `%E6%9E%97%E6%9E%AB.jpeg`, hoverUrl: ``},
+      [
+        {name:'涂强' , url: `%E6%B6%82%E5%BC%BA1.jpg`, hoverUrl: `%E6%B6%82%E5%BC%BA2.jpg`},
+        {name:'陈蔓青' , url: `%E9%99%88%E8%94%93%E9%9D%921.jpg`, hoverUrl: `%E9%99%88%E8%94%93%E9%9D%922.jpg`}
+      ],
       {name:'梁冬' , url: `%E6%A2%81%E5%86%AC1.jpg`, hoverUrl: `%E6%A2%81%E5%86%AC2.jpg`},
       {name:'殷力京' , url: `%E6%AE%B7%E5%8A%9B%E4%BA%AC1.jpg`, hoverUrl: `%E6%AE%B7%E5%8A%9B%E4%BA%AC2.jpg`},
       {name:'焦阳' , url: `%E7%84%A6%E9%98%B31.jpg`, hoverUrl: `%E7%84%A6%E9%98%B32.jpg`},
@@ -122,27 +131,21 @@ module.exports = {
       {name:'王爽' , url: `%E7%8E%8B%E7%88%BD1.jpg`, hoverUrl: `%E7%8E%8B%E7%88%BD2.jpg`},
       {name:'王颖' , url: `%E7%8E%8B%E9%A2%961.jpg`, hoverUrl: `%E7%8E%8B%E9%A2%962.jpg`},
       {name:'罗予恩' , url: `%E7%BD%97%E4%BA%88%E6%81%A91.jpg`, hoverUrl: `%E7%BD%97%E4%BA%88%E6%81%A92.jpg`},
-      [
-        {name:'涂强' , url: `%E6%B6%82%E5%BC%BA1.jpg`, hoverUrl: `%E6%B6%82%E5%BC%BA2.jpg`},
-        {name:'陈蔓青' , url: `%E9%99%88%E8%94%93%E9%9D%921.jpg`, hoverUrl: `%E9%99%88%E8%94%93%E9%9D%922.jpg`}
-      ],
-      [
-        {name:'孙慧扬' , url: `%E5%AD%99%E6%85%A7%E6%89%AC1.jpg`, hoverUrl: `%E5%AD%99%E6%85%A7%E6%89%AC2.jpg`},
-        {name:'花云' , url: `%E8%8A%B1%E4%BA%911.jpg`, hoverUrl: `%E8%8A%B1%E4%BA%912.jpg`}
-      ],
+      {name:'张俊' , url: `%E5%BC%A0%E4%BF%8A1.jpg`, hoverUrl: `%E5%BC%A0%E4%BF%8A2.jpg`},
       {name:'苏伟荣' , url: `%E8%8B%8F%E4%BC%9F%E8%8D%A3.jpg`, hoverUrl: ''},
       [
         {name:'谢郁' , url: `%E8%B0%A2%E9%83%811.jpg`, hoverUrl: `%E8%B0%A2%E9%83%812.jpg`},
         {name:'田光宇' , url: `%E7%94%B0%E5%85%89%E5%AE%871.jpg`, hoverUrl: `%E7%94%B0%E5%85%89%E5%AE%872.jpg`}
       ],
-      {name:'赵晟孜' , url: `%E8%B5%B5%E6%99%9F%E5%AD%9C1.jpg`, hoverUrl: `%E8%B5%B5%E6%99%9F%E5%AD%9C2.jpg`},
-      {name:'empty-item' , url: '', hoverUrl: ''},
-      [
-        {name:'费千哲' , url: `%E8%B4%B9%E5%8D%83%E5%93%B21.jpg`, hoverUrl: `%E8%B4%B9%E5%8D%83%E5%93%B22.jpg`},
-        {name:'周轶景' , url: `%E5%91%A8%E8%BD%B6%E6%99%AF1.jpg`, hoverUrl: `%E5%91%A8%E8%BD%B6%E6%99%AF2.jpg`}
-      ],
-      {name:'车生然' , url: `%E8%BD%A6%E7%94%9F%E7%84%B61.jpg`, hoverUrl: `%E8%BD%A6%E7%94%9F%E7%84%B62.jpg`},
       {name:'郑亮亮' , url: `%E9%83%91%E4%BA%AE%E4%BA%AE1.jpg`, hoverUrl: `%E9%83%91%E4%BA%AE%E4%BA%AE2.jpg`},
+      
+      {name:'林枫' , url: `%E6%9E%97%E6%9E%AB.jpeg`, hoverUrl: ``},
+      {name:'张宇良' , url: `%E5%BC%A0%E5%AE%87%E8%89%AF1.jpg`, hoverUrl: `%E5%BC%A0%E5%AE%87%E8%89%AF2.jpg`},
+      [
+        {name:'车生然' , url: `%E8%BD%A6%E7%94%9F%E7%84%B61.jpg`, hoverUrl: `%E8%BD%A6%E7%94%9F%E7%84%B62.jpg`},
+        {name:'李兴彪' , url: `%E6%9D%8E%E5%85%B4%E5%BD%AA1.jpg`, hoverUrl: `%E6%9D%8E%E5%85%B4%E5%BD%AA2.jpg`}
+      ],
+      {name:'empty-item' , url: '', hoverUrl: ''},
       [
         {name:'' , url: `IMG_3219.jpg`, hoverUrl: `IMG_3216-2.jpg`},
         {name:'' , url: `IMG_3212.jpg`, hoverUrl: `IMG_3216.jpg`}
